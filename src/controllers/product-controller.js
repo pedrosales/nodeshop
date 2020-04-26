2e8725d6a36c9b5ea20c39720184228f0c0da5f5
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-26 00:16:00 -0300

    repository pattern

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
+const repository = require('../repositories/product-repository');
 
 exports.post = (req, res, next) => {
     let contract = new ValidationContract();
@@ -15,8 +16,7 @@ exports.post = (req, res, next) => {
         return;
     }
 
-    var product = new Product(req.body);
-    product.save().then((x) => {
+    repository.create(req.body).then((x) => {
         res.status(201).send({ message: 'Produto cadastrado com sucesso!' });
     }).catch(error => {
         console.log(error);
@@ -25,17 +25,9 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    Product.findByIdAndUpdate(req.params.id, {
-        $set: {
-            title: req.body.title,
-            description: req.body.description,
-            price: req.body.price,
-            slug: req.body.slug
-        }
+    repository.update(req.params.id, req.body).then(x => {
+        res.status(200).send({ message: 'Produto atualizado com sucesso!' });
     })
-        .then(x => {
-            res.status(200).send({ message: 'Produto atualizado com sucesso!' });
-        })
         .catch(error => {
             console.log(error);
             res.status(400).send({ message: 'Falha ao atualizar produto', data: error });
@@ -43,7 +35,7 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-    Product.findOneAndRemove(req.body.id)
+    repository.delete(req.body.id)
         .then(x => {
             res.status(200).send({ message: 'Produto excluido com sucesso!' });
         })
@@ -54,7 +46,7 @@ exports.delete = (req, res, next) => {
 };
 
 exports.get = (req, res, next) => {
-    Product.find({ active: true }, 'title price slug').then(data => {
+    repository.get().then(data => {
         res.status(200).send(data);
     }
     ).catch(error => {
@@ -64,10 +56,7 @@ exports.get = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-    Product.findOne({
-        slug: req.params.slug,
-        active: true
-    }, 'title description price slug tags').then(data => {
+    repository.getBySlug(req.params.slug).then(data => {
         res.status(200).send(data);
     }
     ).catch(error => {
@@ -77,7 +66,7 @@ exports.getBySlug = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-    Product.findById(req.params.id).then(data => {
+    repository.getById(req.params.id).then(data => {
         res.status(200).send(data);
     }
     ).catch(error => {
@@ -87,7 +76,7 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-    Product.find({ tags: req.params.tag, active: true }).then(data => {
+    repository.getByTag(req.params.tag).then(data => {
         res.status(200).send(data);
     }
     ).catch(error => {
