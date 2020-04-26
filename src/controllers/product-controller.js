faee691029e1602919b25a241004cd82c0c2522b
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-25 23:43:42 -0300

    fail fast validations

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,8 +2,19 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator');
 
 exports.post = (req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O Título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.slug, 3, 'O Slug deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.description, 3, 'A descrição deve conter pelo menos 3 caracteres');
+
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     var product = new Product(req.body);
     product.save().then((x) => {
         res.status(201).send({ message: 'Produto cadastrado com sucesso!' });
