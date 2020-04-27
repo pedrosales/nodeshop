29a719feae9a446d66700ac49f965e0c65564799
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-27 16:59:09 -0300

    remove dados sensíveis

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://sa:P3dr09673@api1972-sd6wj.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridKey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=pivo88;AccountKey=lkAB9JzLRVtYWmdVWlaB9MY7FKCo09mux1zKTsoEPXg1fn5kD9eSpRFJFiiL1aQoB9mBPQzKZ3lVbUdXevaqXA==;EndpointSuffix=core.windows.net'
+    connectionString: 'mongoConnectionString',
+    sendgridKey: 'Sendgridkey',
+    containerConnectionString: 'blobConnectionString'
 }
